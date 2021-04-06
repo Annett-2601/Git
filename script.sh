2d8ce588ca1e4d9f3fbc3fa0be4d384c7117c99a
Your Nameannette-2601 <annette-2601@mail.ru>
2021-04-06 23:10:32 +0300

    Скрипт в GITBASH

---
+++ script.sh
@@ -0,0 +1,12 @@
+
+
+#!/bin/bash
+
+mkdir task2
+mkdir folder4 folder4.1  folder4.2
+cd g:QA/VK/folder4.1
+touch 1.txt 2.txt 3.txt 1.json 2.json
+mkdir task7.0 task7.1 task7.2
+ls
+mv {1.txt,2.txt} /g/QA/VK
+
